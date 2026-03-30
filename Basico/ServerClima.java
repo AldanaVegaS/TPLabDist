c1b00f9100c04b98442b4c1f096e99f8c72dcd0a
Dana Contreras <danabcontreras@gmail.com>
2026-03-30 16:35:37 -0300

    feat: ServerClima andando

--- Basico/ServerClima.java
+++ Basico/ServerClima.java
@@ -10,36 +10,53 @@ import java.util.Random;
 public class ServerClima {
     private final static int PORT = 5002;
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(PORT);
-        System.out.println("SH listo...");
 
-        while (true) {
-
-            Socket socket = server.accept();
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-            String fecha = in.readLine().toUpperCase();
-            //System.out.println("SH> Petición recibida: " + fecha);
-        
-
-            String[] predicciones = {
+        String[] predicciones = {
                     "El clima de hoy es soleado con temperaturas agradables.",
                     "Se esperan lluvias ligeras durante la tarde.",
                     "El clima de hoy es nublado con posibilidad de tormentas.",
                     "Se pronostica un día caluroso con alta humedad."
             };
 
-            String respuesta = predicciones[
-                    new Random().nextInt(predicciones.length)
-            ];
+        ServerSocket server;
 
-            out.println(respuesta);
+        try{
+            server = new ServerSocket(PORT);
 
-            socket.close();
+            System.out.println("Servidor Clima> Iniciado correctamente.");
+            System.out.println("Servidor Clima> Escuchando peticiones en el puerto " + PORT + "...\n");
+        } catch (IOException ex) {
+            System.err.println("Servidor Clima> Error al iniciar el servidor: " + ex.getMessage());
+            return;
         }
 
+        Socket socket; //socket para la comunicacion cliente servidor
+
+        while (true) {
+
+            try{
+                socket = server.accept();
+                System.out.println("Servidor Clima> Conexión entrante aceptada.");
+
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+                String fecha = input.readLine().toUpperCase();
+
+                String respuesta = predicciones[new Random().nextInt(predicciones.length)];
+
+                output.flush();
+                output.println(respuesta);
+                
+                System.out.println("Servidor Clima> Pronóstico enviado: \"" + respuesta + "\"");
+            
+                output.close();
+                input.close();
+                socket.close();
+
+            } catch (IOException ex) {
+                System.err.println("Servidor Clima> Error en la comunicación: " + ex.getMessage());
+            }
+        }
     }
 }
