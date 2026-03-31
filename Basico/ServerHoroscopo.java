fcfe8da3c66bc85cfc94e778ea370eb060d80a45
Aldana Vega <aldanavega14@gmail.com>
2026-03-31 20:29:06 -0300

    Mejora en la estructura del código

--- Basico/ServerHoroscopo.java
+++ Basico/ServerHoroscopo.java
@@ -16,40 +16,61 @@ public class ServerHoroscopo {
         "SAGITARIO", "CAPRICORNIO", "ACUARIO", "PISCIS"
     );
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(Config.PORT_HOROSCOPO);
-        System.out.println("SH listo...");
-
-        while (true) {
-
-            Socket socket = server.accept();
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-            String signo = in.readLine().toUpperCase();
-            
-            if (!signosValidos.contains(signo)) {
-                out.println("Signo zodiacal no válido");
-                socket.close();
-                continue;
-            }
-
-            String[] predicciones = {
+    static String[] predicciones = {
                 "Hoy es un buen día para tomar decisiones importantes.",
                 "En el trabajo, la paciencia será tu mejor aliada.",
                 "La salud es tu prioridad, cuida de ti mismo.",
                 "Un encuentro inesperado traerá alegría a tu vida."
-            };
+            }; 
 
-            String respuesta = predicciones[
-                    new Random().nextInt(predicciones.length)
-            ];
+    public static void main(String[] args) throws IOException {
+        ServerSocket server;
 
-            out.println(respuesta);
+        try{
+            server = new ServerSocket(Config.PORT_HOROSCOPO);
 
-            socket.close();
+            System.out.println("Servidor Horoscopo> Iniciado correctamente.");
+            System.out.println("Servidor Horoscopo> Escuchando peticiones en el puerto " + Config.PORT_HOROSCOPO + "...\n");
+        } catch (IOException ex) {
+            System.err.println("Servidor Horoscopo> Error al iniciar el servidor: " + ex.getMessage());
+            return;
+        }
+
+        Socket socket; //socket para la comunicacion cliente servidor
+
+        while (true) {
+
+            try{
+                socket = server.accept();
+                System.out.println("Servidor Horoscopo> Conexión entrante aceptada.");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                String signo = in.readLine().toUpperCase();
+                
+                if (!signosValidos.contains(signo)) {
+                    out.println("Signo zodiacal no válido");
+                    socket.close();
+                    continue;
+                }
+
+                String respuesta = predicciones[
+                        new Random().nextInt(predicciones.length)
+                ];
+
+                out.flush();
+                out.println(respuesta);
+
+                System.out.println("Servidor Horoscopo> Pronóstico enviado: \"" + respuesta + "\"");
+
+                out.close();
+                in.close();
+                socket.close();
+            } catch (IOException ex) {
+                System.err.println("Servidor Horoscopo> Error en la comunicación: " + ex.getMessage());
+            }
+            
         }
 
     }
