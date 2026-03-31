fb8748fb94d5b3f5c6fd598f5bfb853c0f35d320
Dana Contreras <danabcontreras@gmail.com>
2026-03-31 10:41:37 -0300

    Se agrgeo cache a server central, falta implementar algoritmo para eliminar elementos en cache. Probar cache

--- Avanzado/HiloServer.java
+++ Avanzado/HiloServer.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.function.Function;
 
 public class HiloServer extends Thread {
     private Socket socket;
@@ -27,39 +28,44 @@ public class HiloServer extends Thread {
 
             if (request != null) {
 
-                //verificar si el resultado ya está en cache
-                if (ServerCentral.cacheResultados.containsKey(request)) {
-                    System.out.println("HiloServer " + this.idSession + "> Resultado en caché para: " + request);
+                String cacheKey = request.trim().toUpperCase();
 
-                    String responseCache = ServerCentral.cacheResultados.get(request);
+                String responseCache = ServerCentral.cacheResultados.get(cacheKey);
+
+                if (responseCache != null) {
+                    //cache hit, se devuelve el resultado almacenado
+                    System.out.println("HiloServer " + this.idSession + "> Resultado en caché para: " + request);
+                    
+                } else {
+                    responseCache = ServerCentral.cacheResultados.computeIfAbsent(cacheKey, new Function<String, String>() {
+            
+                        public String apply(String key) {
+                            
+                            System.out.println("HiloServer " + idSession + "> Resultado no se encuentra en caché. Consultando servidores...");
+
+                            String[] parts = request.split("\\|");
+
+                            if (parts.length == 2) {
+                                String signo = parts[0];
+                                String fecha = parts[1];
+                                
+                                String horoscopo = ServerCentral.consultarServidorSecundario(Config.PORT_HOROSCOPO, signo);
+                                String clima = ServerCentral.consultarServidorSecundario(Config.PORT_CLIMA, fecha);
+
+                                return "Horóscopo para " + signo + ": " + horoscopo + " | Clima: " + clima;
+                            } else {
+                                return "Error: Formato de solicitud incorrecto.";
+                            }
+                        }
+                    });
 
                     output.flush();
                     output.println(responseCache);
 
-                } else {
-                    //no está en caché, se consulta a los servidores secundarios y se guarda el resultado en cache.
-
-                    System.out.println("HiloServer " + this.idSession + "> Resultado no se encuentra en caché para: " + request);
-
-                    String[] parts = request.split("\\|");
-
-                    if (parts.length == 2) {
-                        String signo = parts[0];
-                        String fecha = parts[1];
-                        
-                        horoscopo = ServerCentral.consultarServidorSecundario(Config.PORT_HOROSCOPO, signo);
-                        clima = ServerCentral.consultarServidorSecundario(Config.PORT_CLIMA, fecha);
-
-                        response = "Horóscopo para " + signo + ": " + horoscopo + " | Clima: " +  clima;
-
-                        output.flush();
-                        output.println(response);
-                    } else {
-                        output.println("HiloServer " + this.idSession + "> Error: Formato de solicitud incorrecto.");
-                    }
+                    System.out.println("HiloServer " + this.idSession + "> Respuesta enviada al cliente: " + responseCache);
                 }
-
             }
+
             output.close();
             input.close();
             socket.close();
