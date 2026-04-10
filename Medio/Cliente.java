c06f218fc300240289d0875f0774c35af5ed5a4b
Dana Contreras <danabcontreras@gmail.com>
2026-04-10 17:55:52 -0300

    Cliente simple y creación de instancias de cliente

--- Medio/Cliente.java
+++ Medio/Cliente.java
@@ -1,18 +1,55 @@
 package Medio;
-
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 public class Cliente {
-    public static void main(String[] args) {
 
-        ArrayList<Thread> clients = new ArrayList<Thread>(); //creación de clientes
+    public static void main(String[] args) throws IOException {
+        boolean exit = false;
+        String signo, fecha, request;
 
-        clients.add(new HiloCliente(1, "Aries", "10/05/2026"));
-        clients.add(new HiloCliente(2, "Tauro", "15/08/2026"));
-        clients.add(new HiloCliente(3, "Geminis", "20/11/2026"));
+        Socket socket; //socket para la comunicacion cliente servidor    
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in)); //para leer lo que escriba el usuario
 
-        for (Thread client : clients) {
-            client.start();
-        }
+        try {            
+            System.out.println("Cliente> Inicio");  
+
+            while(!exit){  
+                
+                System.out.print("Cliente> Ingrese su signo zodiacal (o 'exit' para salir): ");
+                signo = keyboard.readLine();
+                
+                if(signo.equals("exit")) {
+                    exit = true;
+                    System.out.println("Cliente> Fin de programa");
+                    continue; //salta al final del while
+                }
+
+                System.out.print("Cliente> Ingrese la fecha a consultar (dd/mm/aaaa): ");
+                fecha = keyboard.readLine();
+
+                if(!fecha.matches("\\d{2}/\\d{2}/\\d{4}")) {
+                    System.out.println("Cliente> Error: Formato de fecha incorrecto. Use dd/mm/aaaa.\n");
+                    continue; //vuelve a pedir todo desde el principio
+                }
+
+                request = signo + "|" + fecha;
+                socket = new Socket(Config.SERVER, Config.PORT); //abre socket                
+                
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream())); //para leer lo que envie el servidor                 
+                PrintStream output = new PrintStream(socket.getOutputStream()); //para imprimir datos del servidor          
+                         
+                output.println(request); //enviar la petición empaquetada al servidor
+                
+                String st = input.readLine();
+                if(st != null)
+                    System.out.println("Servidor Central> " + st);
+
+                socket.close();
+            }
+
+       } catch (IOException ex) {        
+         System.err.println("Cliente> " + ex.getMessage());   
+       }
     }
 }
